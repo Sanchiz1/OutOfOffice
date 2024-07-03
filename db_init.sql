ccbff0ab2446097b921432b9b22b43fd2bfe10fa
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-07-03 16:56:00 +0300

    Added db init script

---
+++ db_init.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE OutOfOfficeDB;
+
+GO
+
+USE OutOfOfficeDB;
+
+CREATE TABLE Positions (
+    Id INT PRIMARY KEY IDENTITY(1, 1),
+    Name NVARCHAR(Max) NOT NULL
+);
+
+CREATE TABLE Employees (
+    Id INT PRIMARY KEY IDENTITY(1, 1),
+    FullName NVARCHAR(MAX) NOT NULL,
+    PositionId INT FOREIGN KEY REFERENCES Positions(Id) NOT NULL,
+    Subdivision NVARCHAR(Max) NOT NULL,
+    Status NVARCHAR(50) NOT NULL,
+    PeoplePartner INT FOREIGN KEY REFERENCES Employees(Id) NOT NULL,
+    OutOfOfficeBalance INT NOT NULL
+);
+
+CREATE TABLE LeaveRequests (
+    Id INT PRIMARY KEY IDENTITY(1, 1),
+    EmployeeId INT FOREIGN KEY REFERENCES Employees(Id) NOT NULL,
+    AbsenceReason NVARCHAR(Max) NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE NOT NULL,
+    Comment NVARCHAR(MAX),
+    Status NVARCHAR(50) NOT NULL DEFAULT 'New'
+);
+
+CREATE TABLE ApprovalRequests (
+    Id INT PRIMARY KEY IDENTITY,
+    ApproverId INT FOREIGN KEY REFERENCES Employees(Id),
+    LeaveRequestId INT FOREIGN KEY REFERENCES LeaveRequests(Id),
+    Status NVARCHAR(50) NOT NULL DEFAULT 'New',
+    Comment NVARCHAR(MAX)
+);
+
+CREATE TABLE Projects (
+    ID INT PRIMARY KEY IDENTITY(1, 1),
+    ProjectType NVARCHAR(MAX) NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE,
+    ProjectManagerId INT FOREIGN KEY REFERENCES Employees(Id),
+    Comment NVARCHAR(MAX),
+    Status NVARCHAR(50) NOT NULL
+);
