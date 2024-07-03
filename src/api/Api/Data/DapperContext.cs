e912c59211d964beb77a8663c5b90e1f97dc67f9
Sasha Zaitsev <sashazaitsev2005@gmail.com>
2024-07-03 16:47:36 +0300

    InitialCommit

---
+++ src/api/Api/Data/DapperContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Api.Data;
+
+public class DapperContext
+{
+    private readonly string _connectionString;
+    public DapperContext(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("SQLConnection")
+            ?? throw new ArgumentNullException("Connection string not found");
+    }
+    public IDbConnection CreateConnection()
+        => new SqlConnection(_connectionString);
+}
